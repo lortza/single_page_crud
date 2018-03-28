049879d4e3ccc3c629515ea203013343a833dd65
anne <richardson.ae@gmail.com>
2018-03-28 10:05:59 -0500

    Replaces jquery with vanilla js

--- app/helpers/critters_helper.rb
+++ app/helpers/critters_helper.rb
@@ -2,7 +2,7 @@ module CrittersHelper
   def display_description(critter)
     vowels = %w(a e i o u h)
     indefinite_article = 'a'
-    indefinite_article = 'an' if critter.color && vowels.include?(critter.color[0].downcase)
+    indefinite_article = 'an' if critter.color.present? && vowels.include?(critter.color[0].downcase)
 
     preposition = 'in'
     preposition = 'on' if critter.city.name.downcase.match('isla')
